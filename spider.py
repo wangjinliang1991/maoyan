158474abd3784b234a5d028733294b9db4fcc841
wangjinliang1991 <632180350@qq.com>
2018-10-15 16:36:40 +0800

    work well, second time

--- spider.py
+++ spider.py
@@ -1,18 +1,53 @@
+import json
+import re
+import time
+
 import requests
 from requests.exceptions import RequestException
+
+
 def get_one_page(url):
     try:
-        response = requests.get(url)
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)Chrome/69.0.3497.100 Safari/537.36'
+        }
+        response = requests.get(url,headers=headers)
         if response.status_code == 200:
             return response.text
         return None
     except RequestException:
         return None
 
-def main():
-    url = 'http://maoyan.com/board/4'
-    html = get_one_page(url)
-    print(html)
 
-if __name__=='--main':
-    main()
+
+def parse_one_page(html):
+    pattern = re.compile('<dd>.*?board-index.*?>(\d+)</i>.*?data-src="(.*?)".*?name"><a'
+                         + '.*?>(.*?)</a>.*?star">(.*?)</p>.*?releasetime">(.*?)</p>'
+                         + '.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
+    items = re.findall(pattern, html)
+    for item in items:
+        yield {
+            'index': item[0],
+            'image': item[1],
+            'title': item[2],
+            'actor': item[3].strip()[3:],
+            'time': item[4].strip()[5:],
+            'score': item[5] + item[6]
+        }
+
+# json.dump()实现字典的序列化，ensure_ascii=False保证输出结果是中文形式而非Unicode编码
+def write_to_file(content):
+    with open('result.txt', 'a', encoding='utf-8') as f:
+        f.write(json.dumps(content, ensure_ascii=False) + '\n')
+
+def main(offset):
+    url = 'http://maoyan.com/board/4?offset=' + str(offset)
+    html =  get_one_page(url)
+    for item in parse_one_page(html):
+        print(item)
+        write_to_file(item)
+
+if __name__ == '__main__':
+    for i in range(10):
+        main(offset=i*10)
+        time.sleep(1)
